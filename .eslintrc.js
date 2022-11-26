c0f0d79e17d04f16926f265a8e1ca2e934b93402
Joshua Semana <joshuasemana@BallsInspectors-MacBook-Pro.local>
2022-11-26 09:41:45 -0700

    finally got the linting to a stable level

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "standard-with-typescript", "prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "prettier"
+  ],
   overrides: [],
   parserOptions: {
     project: "./tsconfig.json",
@@ -13,5 +18,19 @@ module.exports = {
   plugins: ["react"],
   rules: {
     "@typescript-eslint/no-empty-interface": 0,
+    "react/jsx-indent": ["error", 2],
+    "react/jsx-indent-props": ["error", 2],
+    "react/jsx-closing-tag-location": 2,
+    "react/jsx-one-expression-per-line": ["error", { "allow": "single-child" }],
+    "react/jsx-wrap-multilines": ["error", {
+      "declaration": "parens-new-line",
+       "assignment": "parens-new-line",
+       "return": "parens-new-line",
+       "arrow": "parens-new-line",
+       "condition": "parens-new-line",
+       "logical": "parens-new-line",
+       "prop": "parens-new-line"
+     },
+   ]
   },
 };
